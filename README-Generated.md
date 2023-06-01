d6f000b754017995da9885ed346baca7926c73e4
juanx198522 <juanx1985@icloud.com>
2023-06-01 00:18:46 -0400

    Last commit

--- README-Generated.md
+++ README-Generated.md
@@ -24,15 +24,12 @@
 
 ## License
 
-  ![badge](https://img.shields.io/badge/license-None-blue)
+  ![badge](https://img.shields.io/badge/license-MIT-blue)
     
 
-  [None](https://choosealicense.com/licenses/None)
+  This project is licensed under the MIT license. 
+  [MIT](https://choosealicense.com/licenses/mit)
     
-
   
-  [None](https://choosealicense.com/licenses/None)
-    
-    
 
 ---
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { 
-  if (license !== 'no license') {
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
     return `
   ![badge](https://img.shields.io/badge/license-${license}-blue)
     `;
@@ -12,9 +12,9 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license !== 'no license') {
-  return `
-  [${license}](https://choosealicense.com/licenses/${license})
+  if (license !== 'None') {
+    return `
+  [${license}](https://choosealicense.com/licenses/${license.toLowerCase()})
     `;
   } else {
     return ' ';
@@ -23,15 +23,14 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license !== 'no license') {
-  return `
-  ${renderLicenseLink(license)}
-    `;
+  if (license !== 'None') {
+    return `
+  This project is licensed under the ${license} license. ${renderLicenseLink(license)}
+  `;
   } else {
     return ' ';
   }
- }
-
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.projectName}
@@ -60,7 +59,6 @@ ${data.collaborators}
 
 ## License
 ${renderLicenseBadge(data.license)}
-${renderLicenseLink(data.license)}
 ${renderLicenseSection(data.license)}
 
 ---`;
